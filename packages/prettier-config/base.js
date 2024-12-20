3449512b3bc86a54d140f32fc31876b925c582b8
Sparsh Agarwal <sparsh.a@coindcx.com>
2024-12-20 16:48:14 +0530

    Refactor Prettier configuration and update package exports

--- packages/prettier-config/base.js
+++ packages/prettier-config/base.js
@@ -1,9 +1,10 @@
 /**
+ * A shared Prettier configuration for the repository.
+ *
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
- */
-
-module.exports = {
+ * */
+export const config = {
   semi: true,
   singleQuote: true,
   trailingComma: "all",
