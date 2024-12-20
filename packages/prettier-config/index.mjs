a338d6bf0e2bf5143986768bfb4c2e4c7d82bb00
Sparsh Agarwal <sparsh.a@coindcx.com>
2024-12-20 16:14:49 +0530

    Add ESLint and TypeScript configuration packages with initial setup

---
+++ packages/prettier-config/index.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+};
