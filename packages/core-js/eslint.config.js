3e394bc8d62b3a84782ab2b0f3c53803c52c2962
Sparsh Agarwal <sparsh.a@coindcx.com>
2024-12-23 11:03:30 +0530

    Add check-exports script and update ESLint configuration for core-js package

--- packages/core-js/eslint.config.js
+++ packages/core-js/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from '@okto-sdk/eslint-config/next-js';
+import { config } from "@okto-sdk/eslint-config/base";
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default config;
