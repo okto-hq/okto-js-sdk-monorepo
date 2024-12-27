3d5b932de303c172833677e6549efaa2f84c9399
Sparsh Agarwal <sparsh.a@coindcx.com>
2024-12-27 18:16:03 +0530

    Add axios and axios-retry dependencies; implement gateway and BFF clients with request interceptors

---
+++ packages/core-js/src/api/client.ts
@@ -0,0 +1,54 @@
+import { globalConfig } from '@/config/index.js';
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+
+const gatewayClient = axios.create({
+  baseURL: globalConfig.env.gatewayBaseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+gatewayClient.interceptors.request.use(
+  (config) => {
+    if (config.headers['Skip-Authorization'] === true) {
+      return config;
+    }
+
+    config.headers.setAuthorization(
+      `Bearer ${globalConfig.authOptions?.userSessionKey}`,
+    );
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+const bffClient = axios.create({
+  baseURL: globalConfig.env.bffBaseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+bffClient.interceptors.request.use(
+  (config) => {
+    config.headers.setAuthorization(
+      `Bearer ${globalConfig.authOptions?.userSessionKey}`,
+    );
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosRetry(bffClient, {
+  retries: 3,
+  retryDelay: axiosRetry.exponentialDelay,
+});
+
+export { bffClient, gatewayClient };
