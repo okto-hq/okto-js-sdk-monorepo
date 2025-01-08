bac9102b53618a45ead8ac1e8d7be840953be70e
SrijanCoinDCX <srijan.samridh@coindcx.com>
2025-01-08 15:03:25 +0530

    refactor: add session verification and order estimation methods in BffClientRepositor

--- packages/core-js/src/chains/index.ts
+++ packages/core-js/src/chains/index.ts
@@ -1,3 +1,22 @@
-class Chain {}
+import BffClientRepository from "@/api/bff";
+
+class Chain {
+  // -------------------- Callbacks Definitions -------------------- //
+  private _getSessionKey: () => string | undefined;
+
+  // -------------------- Public Methods -------------------- //
+  async getChains() {
+    const sessionKey = this._getSessionKey();
+    if (!sessionKey) {
+      throw new Error('Session Key is not set');
+    }
+
+    const response = await BffClientRepository.getSupportedNetworks();
+
+    //TODO: Check if the response is valid
+
+    return response; // {getSupportedNetworks}
+  }
+}
 
 export default Chain;
--- packages/core-js/src/tokens/index.ts
+++ packages/core-js/src/tokens/index.ts
@@ -1,3 +1,36 @@
-class Token {}
+import BffClientRepository from "@/api/bff";
+
+class Token {
+  // -------------------- Callbacks Definitions -------------------- //
+  private _getSessionKey: () => string | undefined;
+
+  // -------------------- Public Methods -------------------- //
+  async getTokens() {
+    const sessionKey = this._getSessionKey();
+    if (!sessionKey) {
+      throw new Error('Session Key is not set');
+    }
+
+    const response = await BffClientRepository.getSupportedTokens();
+
+    //TODO: Check if the response is valid
+
+    return response; // {getSupportedNetworks}
+  }
+
+  async getNftCollections() {
+    const sessionKey = this._getSessionKey();
+    if (!sessionKey) {
+      throw new Error('Session Key is not set');
+    }
+
+    const response = await BffClientRepository.getNftOrderDetails();
+
+    //TODO: Check if the response is valid
+
+    return response; // {getSupportedNetworks}
+  }
+
+}
 
 export default Token;
