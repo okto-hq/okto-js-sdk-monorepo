2dd270f05efda988349b4bc19d717a10b291cb9b
Sparsh Agarwal <sparsh.a@coindcx.com>
2025-01-08 11:58:59 +0530

    feat: enhance UserOp type with detailed properties and add Address and uint256 types

--- packages/core-js/src/types/core.ts
+++ packages/core-js/src/types/core.ts
@@ -1,6 +1,8 @@
 export type ByteArray = Uint8Array;
+export type Address = `0x${string}`;
 export type Hex = `0x${string}`;
 export type Hash = `0x${string}`;
+export type uint256 = bigint;
 
 export type User = {
   userId: string;
@@ -11,17 +13,34 @@ export type User = {
 };
 
 export type UserOp = {
-  sender: string;
-  nonce: string;
-  initCode: string;
-  callData: string;
-  signature: string;
-  maxFeePerGas: string;
-  maxPriorityFeePerGas: string;
-  verificationGasLimit: string;
-  callGasLimit: string;
-  preVerificationGas: string;
-  paymasterAndData: string;
+  /** The data to pass to the `sender` during the main execution call. */
+  callData: Hex;
+  /** The amount of gas to allocate the main execution call */
+  callGasLimit: uint256;
+  /** Account factory. Only for new accounts. */
+  factory?: Address | undefined;
+  /** Data for account factory. */
+  factoryData?: Hex | undefined;
+  /** Maximum fee per gas. */
+  maxFeePerGas: uint256;
+  /** Maximum priority fee per gas. */
+  maxPriorityFeePerGas: uint256;
+  /** Anti-replay parameter. */
+  nonce: uint256;
+  /** Address of paymaster contract. */
+  paymaster?: Address | undefined;
+  /** Data for paymaster. */
+  paymasterData?: Hex | undefined;
+  /** The amount of gas to allocate for the paymaster post-operation code. */
+  paymasterPostOpGasLimit?: uint256 | undefined;
+  /** The amount of gas to allocate for the paymaster validation code. */
+  paymasterVerificationGasLimit?: uint256 | undefined;
+  /** Extra gas to pay the Bundler. */
+  preVerificationGas: uint256;
+  /** The account making the operation. */
+  sender: Address;
+  /** Data passed into the account to verify authorization. */
+  signature: Hex;
+  /** The amount of gas to allocate for the verification step. */
+  verificationGasLimit: uint256;
 };
-
-// export type UserOp = RpcUserOperation<'0.7'>;
