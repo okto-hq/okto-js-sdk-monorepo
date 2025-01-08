5aa61917f404b2305d90b0ed73038b7e0b84dd05
Sparsh Agarwal <sparsh.a@coindcx.com>
2025-01-08 11:29:37 +0530

    feat: refactor core types and move legacy types to old.ts

--- packages/core-js/src/types/core.ts
+++ packages/core-js/src/types/core.ts
@@ -1,47 +1,6 @@
-export type ChainType = 'EVM' | 'SVM' | 'APT';
-
-// TODO(sparsh.a): This is a placeholder for the actual implementation
-export type Address = string;
-
-export type Network = {
-  networkId: string;
-  caipBlockchainId: string;
-  name: string | undefined;
-  sponsorshipEnabled: boolean | undefined;
-  whitelisted: boolean | undefined;
-  type: ChainType | undefined;
-  onRampEnabled: boolean | undefined;
-  gsnEnabled: boolean | undefined;
-};
-
-export type Wallet = {
-  network: Network;
-  address: Address;
-};
-
-export type Token = {
-  tokenId: string;
-  tokenAddress: string;
-  network: Network;
-  whitelisted: boolean | undefined;
-  onRampEnabled: boolean | undefined;
-};
-
-export type NftCollection = {
-  nftCollectionId: string;
-  collectionAddress: string;
-  network: Network;
-  whitelisted: boolean | undefined;
-  ercType: 'ERC721' | 'ERC1155';
-};
-
-export type NftMetadata = {
-  uri: string;
-  nftName: string;
-  description: string;
-};
-
-// ---
+export type ByteArray = Uint8Array;
+export type Hex = `0x${string}`;
+export type Hash = `0x${string}`;
 
 export type User = {
   userId: string;
@@ -50,3 +9,19 @@ export type User = {
   eddsaPublicKey: string;
   sessionExpiry: string;
 };
+
+export type UserOp = {
+  sender: string;
+  nonce: string;
+  initCode: string;
+  callData: string;
+  signature: string;
+  maxFeePerGas: string;
+  maxPriorityFeePerGas: string;
+  verificationGasLimit: string;
+  callGasLimit: string;
+  preVerificationGas: string;
+  paymasterAndData: string;
+};
+
+// export type UserOp = RpcUserOperation<'0.7'>;
