cd13c5004b828c6b3d9dae93a08e6ab1bfd05105
Sparsh Agarwal <sparsh.a@coindcx.com>
2025-01-16 13:43:03 +0530

    feat: set up test-core-js application with TypeScript, ESLint, and Prettier configurations

---
+++ apps/test-core-js/prettier.config.js
@@ -0,0 +1,7 @@
+import { config } from "@okto-sdk/prettier-config/base";
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default config;
