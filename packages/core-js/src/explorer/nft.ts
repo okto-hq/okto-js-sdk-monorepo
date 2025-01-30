da6fd032699646824f565e41095ee59d34041225
Sparsh Agarwal <sparsh.a@coindcx.com>
2025-01-30 14:10:36 +0530

    feat: add TODO for implementing NFT collection fetching

--- packages/core-js/src/explorer/nft.ts
+++ packages/core-js/src/explorer/nft.ts
@@ -4,7 +4,9 @@ import type OktoClient from '@/core/index.js';
 /**
  * Fetches NFT collection details from the backend.
  */
-export async function getNftCollections(oc: OktoClient) {
+// TODO: Implement the function to fetch NFT collection details using /orders with a filter.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function getNftCollections(oc: OktoClient) {
   try {
     const response = await BffClientRepository.getNftOrderDetails(oc);
     return response;
