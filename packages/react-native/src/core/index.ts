81f3cf77108153fcd9821f0bd38002c492f11c86
karansingla517 <karan.singla@coindcx.com>
2025-02-11 14:07:22 +0530

    chore: chore: bump version to 0.0.0-dev.48 and update session storage to use AsyncStorage

--- packages/react-native/src/core/index.ts
+++ packages/react-native/src/core/index.ts
@@ -6,8 +6,7 @@ import type { RpcError } from '@okto_web3/core-js-sdk/errors';
 import type { Address, AuthData } from '@okto_web3/core-js-sdk/types';
 import type { SessionConfig } from '@okto_web3/core-js-sdk/core';
 import { decryptData, encryptData } from '../utils/encryptionUtils.js';
-import * as Keychain from 'react-native-keychain';
-
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
 class OktoClient extends OktoCoreClient {
   private _vendorPrivKey: string;
 
@@ -16,40 +15,35 @@ class OktoClient extends OktoCoreClient {
     this._vendorPrivKey = config.vendorPrivKey;
   }
 
-  override loginUsingOAuth(
+  override async loginUsingOAuth(
     data: AuthData,
   ): Promise<Address | RpcError | undefined> {
-    return super.loginUsingOAuth(data, (session) => {
-      console.log('Session before encryption:', session);
-      const encryptedSession = encryptData(session, this._vendorPrivKey);
-      console.log('Encrypted session:', encryptedSession);
-
-      Keychain.setGenericPassword('session', encryptedSession)
-        .then(() => console.log('Session stored successfully'))
-        .catch((error) =>
-          console.error('Failed to store session in Keychain:', error),
-        );
+    return super.loginUsingOAuth(data, async (session) => {
+      await AsyncStorage.default.setItem(
+        'session',
+        encryptData(session, this._vendorPrivKey),
+      );
     });
   }
 
   override getSessionConfig(): SessionConfig | undefined {
     let sessionConfig: SessionConfig | undefined;
 
-    Keychain.getGenericPassword()
-      .then((credentials) => {
-        if (credentials && credentials.password) {
+    AsyncStorage.default.getItem('session')
+      .then((encryptedSession: string | null) => {
+        if (encryptedSession) {
           sessionConfig = decryptData<SessionConfig>(
-            credentials.password,
+            encryptedSession,
             this._vendorPrivKey,
           );
         }
       })
-      .catch((error) =>
-        console.error('Failed to retrieve session from Keychain:', error),
-      );
+      .catch((error: string) => {
+        console.error('Failed to get session from storage:', error);
+      });
 
     return sessionConfig;
   }
-}
 
+}
 export { OktoClient };
