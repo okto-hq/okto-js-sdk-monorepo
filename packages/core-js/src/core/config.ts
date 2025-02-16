59c135f311a440e24d2cd46ccb4c14f0aa0ab447
karansingla517 <karan.singla@coindcx.com>
2025-02-17 00:48:07 +0530

    fix: fix: remove redundant comment from job manager address in production environment configuration

--- packages/core-js/src/core/config.ts
+++ packages/core-js/src/core/config.ts
@@ -12,6 +12,6 @@ export const productionEnvConfig: EnvConfig = {
   gatewayBaseUrl: 'https://okto-gateway.okto.tech',
   bffBaseUrl: 'https://apigw.okto.tech',
   paymasterAddress: '0x0871051BfF8C7041c985dEddFA8eF63d23AD3Fa0',
-  jobManagerAddress: '0xED3D17cae886e008D325Ad7c34F3bdE030B80c2E', // job manager address //
+  jobManagerAddress: '0xED3D17cae886e008D325Ad7c34F3bdE030B80c2E',
   chainId: 24879,
 };
