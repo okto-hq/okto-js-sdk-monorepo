424561bf004170db2a5d0c73243176e4a08f4f11
Sparsh Agarwal <sparsh.a@coindcx.com>
2025-02-17 15:17:56 +0530

    fix: refactor environment configuration to use constants

--- packages/core-js/src/core/config.ts
+++ packages/core-js/src/core/config.ts
@@ -4,19 +4,15 @@ import type { EnvConfig } from './types.js';
 export const sandboxEnvConfig: EnvConfig = {
   gatewayBaseUrl: 'https://okto-gateway.oktostage.com',
   bffBaseUrl: 'https://apigw.oktostage.com',
-  paymasterAddress: Constants.ENV_CONFIG.SANDBOX
-    .PAYMASTER_ADDRESS as `0x${string}`,
-  jobManagerAddress: Constants.ENV_CONFIG.SANDBOX
-    .JOB_MANAGER_ADDRESS as `0x${string}`,
-  chainId: 24879,
+  paymasterAddress: Constants.ENV_CONFIG.SANDBOX.PAYMASTER_ADDRESS,
+  jobManagerAddress: Constants.ENV_CONFIG.SANDBOX.JOB_MANAGER_ADDRESS,
+  chainId: Constants.ENV_CONFIG.SANDBOX.CHAIN_ID,
 };
 
 export const productionEnvConfig: EnvConfig = {
   gatewayBaseUrl: 'https://okto-gateway.okto.tech',
   bffBaseUrl: 'https://apigw.okto.tech',
-  paymasterAddress: Constants.ENV_CONFIG.PRODUCTION
-    .PAYMASTER_ADDRESS as `0x${string}`,
-  jobManagerAddress: Constants.ENV_CONFIG.PRODUCTION
-    .JOB_MANAGER_ADDRESS as `0x${string}`,
-  chainId: 24879,
+  paymasterAddress: Constants.ENV_CONFIG.PRODUCTION.PAYMASTER_ADDRESS,
+  jobManagerAddress: Constants.ENV_CONFIG.PRODUCTION.JOB_MANAGER_ADDRESS,
+  chainId: Constants.ENV_CONFIG.PRODUCTION.CHAIN_ID,
 };
