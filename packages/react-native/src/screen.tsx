6e7d03c087cccd79739468a869693f80733e551b
karansinglacoindcx <karan.singla@coindcx.com>
2025-04-09 13:25:07 +0530

    feat: feat: add resolutions for type definitions and refactor WebViewScreen component

--- packages/react-native/src/screen.tsx
+++ packages/react-native/src/screen.tsx
@@ -1,23 +1,19 @@
-// src/components/WebViewScreen/WebViewScreen.tsx
-import React from 'react';
-import { WebView } from 'react-native-webview';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-
-const CustomWebView = WebView as unknown as React.ComponentType<any>;
+import { SafeAreaView } from "react-native";
+import {WebView} from "react-native-webview";
 
 interface WebViewScreenProps {
   url: string;
 }
 
+
 export const WebViewScreen = ({ url }: WebViewScreenProps) => {
   return (
-    // <SafeAreaView style={{ flex: 1 }}>
-      <CustomWebView 
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView  
         source={{ uri: url }}
         startInLoadingState={true}
         javaScriptEnabled={true}
       />
-    // {/* </SafeAreaView> */}
+    // </SafeAreaView>
   );
 };
