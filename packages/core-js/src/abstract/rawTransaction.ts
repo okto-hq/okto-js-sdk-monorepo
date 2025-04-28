950d0587c774463770c5b35174417a6fc1ffad7e
karansinglacoindcx <karan.singla@coindcx.com>
2025-04-28 16:09:46 +0530

    feat: add optional fee payer address to NFT and token transfer functions

--- packages/core-js/src/abstract/rawTransaction.ts
+++ packages/core-js/src/abstract/rawTransaction.ts
@@ -1,6 +1,7 @@
 import type OktoClient from '@/core/index.js';
 import { evmRawTransaction as useropgen } from '@/userop/rawTransaction.js';
 import type { RawTransactionIntentParams } from '@/userop/types.js';
+import type { Address } from '@/types/core.js';
 
 /**
  * Do a Raw Transaction on any EVM Chain.
@@ -8,8 +9,9 @@ import type { RawTransactionIntentParams } from '@/userop/types.js';
 export async function evmRawTransaction(
   oc: OktoClient,
   data: RawTransactionIntentParams,
+  feePayerAddress?: Address
 ): Promise<string> {
-  const userop = await useropgen(oc, data);
+  const userop = await useropgen(oc, data, feePayerAddress);
   const signedUserOp = await oc.signUserOp(userop);
   const res = await oc.executeUserOp(signedUserOp);
   return res;
