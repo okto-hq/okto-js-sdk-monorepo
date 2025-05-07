cec3ca9aa34d5c158c1f2fa2ece850310e880606
karansinglacoindcx <karan.singla@coindcx.com>
2025-05-08 03:55:23 +0530

    feat: feat: update AUTH_PAGE_URL to point to the sandbox onboarding environment

--- packages/core-js/src/utils/constants.ts
+++ packages/core-js/src/utils/constants.ts
@@ -46,7 +46,7 @@ export class Constants {
       ENTRYPOINT_CONTRACT_ADDRESS:
         '0xCa5b1b0d3893b5152014fD5B519FF50f7C40f9da' as Hex,
       CHAIN_ID: 1802466136,
-      AUTH_PAGE_URL: 'https://onboarding.oktostage.com/', //Need to be changed
+      AUTH_PAGE_URL: 'https://sandbox-onboarding.okto.tech/',
     },
     // PRODUCTION: {
     //   PAYMASTER_ADDRESS: '0x0871051BfF8C7041c985dEddFA8eF63d23AD3Fa0' as Hex,
