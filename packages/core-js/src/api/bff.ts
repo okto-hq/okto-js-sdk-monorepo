e684cf66217dd9d25d5412db1c468a0b29974041
SrijanCoinDCX <srijan.samridh@coindcx.com>
2025-06-02 15:07:56 +0530

    feat: add rawRead function to BffClientRepository and expose it in explorers

--- packages/core-js/src/api/bff.ts
+++ packages/core-js/src/api/bff.ts
@@ -6,6 +6,8 @@ import type {
   Order,
   OrderEstimateResponse,
   OrderFilterRequest,
+  ReadContractPayload,
+  ReadContractResponse,
   UserNFTBalance,
   UserPortfolioActivity,
   UserPortfolioData,
@@ -31,6 +33,7 @@ class BffClientRepository {
     // POST
     estimateOrder: '/api/oc/v1/estimate',
     verifySession: '/api/oc/v1/verify-session',
+    rawRead: '/api/oc/v1/readContractData',
   };
 
   /**
@@ -216,6 +219,29 @@ class BffClientRepository {
 
     if (!response.data.data) {
       throw new Error('Response data is missing');
+    }
+
+    return response.data.data;
+  }
+
+  /**
+   * Reads data from a smart contract using the BFF API.
+   *
+   * @param oc - The OktoClient instance.
+   * @param requestBody - The updated request body containing advanced contract read parameters.
+   * @returns ReadContractResponse the contract read response.
+   * @throws Error if the read fails or throws error.
+   */
+  public static async rawRead(
+    oc: OktoClient,
+    requestBody: ReadContractPayload,
+  ): Promise<ReadContractResponse | undefined> {
+    const response = await getBffClient(oc).post<
+      ApiResponse<ReadContractResponse>
+    >(this.routes.rawRead, requestBody);
+
+    if (response.data.status === 'error') {
+      throw new Error('Failed to read contract data');
     }
 
     return response.data.data;
