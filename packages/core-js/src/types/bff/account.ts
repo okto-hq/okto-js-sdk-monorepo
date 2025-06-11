d48e4bfafc9f6a8078b75a614d0b3442504be734
Sparsh Agarwal <sparsh.a@coindcx.com>
2025-06-11 13:27:09 +0530

    Add Raw Read functionality (#142)
    * backmerge v1.0.1 (#141)
    
    * chore: update package versions and changelogs for core-js, react, and react-native SDKs
    
    * chore: update package versions to 1.0.0 for core-js, react, and react-native SDKs
    
    * feat: add additional allowed origins for the webview in react-sdk (#139)
    
    * feat: add additional allowed origins for the webview in react-sdk
    
    * Update constants.ts (#140)
    
    removed the duplicate domain.
    
    * chore: update version to 1.0.0-rc.1 for core-js, react, and react-native SDKs
    
    * chore: update package versions to 1.0.1 for core-js, react, and react-native SDKs
    
    ---------
    
    Co-authored-by: Srijan Samridh <166486637+SrijanCoinDCX@users.noreply.github.com>
    
    * feat: add rawRead function to BffClientRepository and expose it in explorers (#135)
    
    * feat: add rawRead function to BffClientRepository and expose it in explorers
    
    * feat: enhance ReadContractPayload type to support function calls with type arguments
    
    * chore: update package versions to 1.0.1-dev.1 for core-js-sdk, react-native-sdk, and react-sdk
    
    * feat: add Raw Read functionality for direct blockchain data access
    
    * feat: add Raw Read functionality across core-js, react, and react-native SDKs
    
    ---------
    
    Co-authored-by: Srijan Samridh <166486637+SrijanCoinDCX@users.noreply.github.com>

--- packages/core-js/src/types/bff/account.ts
+++ packages/core-js/src/types/bff/account.ts
@@ -299,3 +299,60 @@ export type OrderEstimateResponse = {
     };
   };
 };
+
+/**
+ * ========================
+ * Read Contract Types
+ * ========================
+ */
+
+/**
+ * Represents the payload for reading a contract.
+ */
+export type ReadContractPayload =
+  | {
+      caip2Id: string;
+      data: {
+        function: string;
+        typeArguments: string[];
+        functionArguments: string[];
+      };
+    } // For reading a contract function with type arguments - Aptos
+  | {
+      caip2Id?: string;
+      data?: {
+        contractAddress?: string;
+        abi?: {
+          inputs?: Array<{
+            internalType?: string;
+            name?: string;
+            type?: string;
+          }>;
+          name?: string;
+          outputs?: Array<{
+            internalType?: string;
+            name?: string;
+            type?: string;
+          }>;
+          stateMutability?: string;
+          type?: string;
+        };
+        args?: Record<string, unknown>;
+      };
+    };
+
+export type ReadContractResponse =
+  | {
+      status: 'success';
+      data: string[];
+    }
+  | {
+      status: 'error';
+      error: {
+        code: number;
+        errorCode: string;
+        message: string;
+        trace_id?: string;
+        details?: string;
+      };
+    };
